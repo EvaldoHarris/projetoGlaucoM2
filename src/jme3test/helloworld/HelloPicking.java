113ce7134c9e8e34187bd4fa2809ab2c169edd91
EvaldoHarris <evaldo.joaoj@hotmail.com>
2019-06-10 21:01:30 -0300

    alteracoes

--- src/jme3test/helloworld/HelloPicking.java
+++ src/jme3test/helloworld/HelloPicking.java
@@ -12,6 +12,7 @@ import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
 import com.jme3.math.Ray;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
@@ -21,6 +22,7 @@ import com.jme3.scene.shape.Box;
 import com.jme3.scene.shape.Sphere;
 import com.jme3.texture.Texture;
 import com.jme3.util.SkyFactory;
+import java.util.ArrayList;
 
 public class HelloPicking extends SimpleApplication {
 
@@ -37,6 +39,9 @@ public class HelloPicking extends SimpleApplication {
     boolean dragon = true;
     float z = -100f,x = -20f, y = 0f;
     Geometry cube;
+    private Node ninjas;
+    private boolean isRunning = true;
+    int cont = 1;
 
     @Override
     public void simpleInitApp() {
@@ -58,15 +63,15 @@ public class HelloPicking extends SimpleApplication {
         shootables3 = new Node("Shootables3");
         shootables4 = new Node("Shootables4");
         shootables5 = new Node("Shootables4");
-        
+        ninjas = new Node("Ninjas");
 
         shootables.attachChild(makeCube("a Dragon", x, y, z));
         rootNode.attachChild(shootables);
-        shootables2.attachChild(makeCube("the Sheriff", 20f, 1f, -100f));
+        shootables2.attachChild(makeCube("the Sheriff", 20f, 1f, z));
         rootNode.attachChild(shootables2);
-        shootables3.attachChild(makeCube("a tin can", 13f, -2f, -100f));
+        shootables3.attachChild(makeCube("a tin can", 13f, -2f, z));
         rootNode.attachChild(shootables3);
-        shootables4.attachChild(makeCube("the Deputy", 25f, 0f, -100f));
+        shootables4.attachChild(makeCube("the Deputy", 25f, 0f, z));
         rootNode.attachChild(shootables4);
         shootables.detachChild(makeFloor());
         shootables5.attachChild(makeCharacter());
@@ -74,14 +79,20 @@ public class HelloPicking extends SimpleApplication {
         shootables2.detachChild(makeFloor());
         shootables3.detachChild(makeFloor());
         shootables4.detachChild(makeFloor());
+        
+                
+
+        rootNode.attachChild(ninjas);
+                
+                
 
     }
 
     void deleta(String hit) {
         System.out.println("deleta()");
-        if (hit == "Oto-geom-1") {
-            shootables5.detachChild(makeCharacter());
-            rootNode.detachChild(shootables5);
+        if (hit == "Ninja-Oto-geom-1") {
+            System.out.println("deleta ninja");
+            
         }
         if (hit == "a Dragon") {
             shootables.detachChild(makeCube("a Dragon", -2f, 0f, 1f));
@@ -112,7 +123,9 @@ public class HelloPicking extends SimpleApplication {
         inputManager.addMapping("Shoot",
                 new KeyTrigger(KeyInput.KEY_SPACE), // trigger 1: spacebar
                 new MouseButtonTrigger(MouseInput.BUTTON_LEFT)); // trigger 2: left-button click
-        inputManager.addListener(actionListener, "Shoot");
+         inputManager.addMapping("Pause", new KeyTrigger(KeyInput.KEY_P));
+        inputManager.addMapping("CriaNinja", new KeyTrigger(KeyInput.KEY_G));
+        inputManager.addListener(actionListener, new String[]{"Pause", "CriaNinja", "Shoot"});
     }
     /**
      * Definindo a ação "Atirar": Determine o que foi atingido e como responder.
@@ -120,6 +133,59 @@ public class HelloPicking extends SimpleApplication {
     private ActionListener actionListener = new ActionListener() {
 
         public void onAction(String name, boolean keyPressed, float tpf) {
+            if (name.equals("Pause") && !keyPressed) {
+                isRunning = !isRunning;
+            }
+
+            if (isRunning) {
+                if (name.equals("CriaNinja") && keyPressed) {
+                   
+                    Spatial ninja = createNinja(Integer.toString(cont));
+                    ninjas.attachChild(ninja);
+                    cont++;
+                }
+                if (name.equals("RemoveNinjas") && keyPressed) {
+                    for (Spatial s : ninjas.getChildren()) {
+                        rootNode.detachChild(s);
+                    }
+                    ninjas.detachAllChildren();
+                }
+                if (name.equals("RemoveOlder") && keyPressed) {
+                    int menor;
+                    ArrayList<Integer> listaNum = new ArrayList<Integer>();
+
+                    for (Spatial s : ninjas.getChildren()) {
+                        listaNum.add(Integer.parseInt(s.getName()));
+                    }
+
+                    menor = listaNum.get(0);
+                    for (int n : listaNum) {
+                        if (n < menor) {
+                            menor = n;
+                        }
+                    }
+
+                    ninjas.detachChildNamed(Integer.toString(menor));
+                    rootNode.detachChildNamed(Integer.toString(menor));
+                }
+                if (name.equals("DoubleSizeNewer") && keyPressed) {
+                    int maior = 0;
+                    ArrayList<Integer> listaNum = new ArrayList<Integer>();
+
+                    for (Spatial s : ninjas.getChildren()) {
+                        listaNum.add(Integer.parseInt(s.getName()));
+                    }
+
+                    for (int n : listaNum) {
+                        if (n > maior) {
+                            maior = n;
+                        }
+                    }
+                    rootNode.getChild(Integer.toString(maior)).scale(2);
+                }
+            } else {
+                System.out.println("Aperte P para voltar à aplicação");
+            }
             if (name.equals("Shoot") && !keyPressed) {
                 // 1. Redefinir lista de resultados.
                 CollisionResults results = new CollisionResults();
@@ -130,6 +196,7 @@ public class HelloPicking extends SimpleApplication {
                 shootables2.collideWith(ray, results);
                 shootables3.collideWith(ray, results);
                 shootables4.collideWith(ray, results);
+                ninjas.collideWith(ray, results);
                 // 4. Imprimir os resultados
                 System.out.println("----- Collisions? " + results.size() + "-----");
                 for (int i = 0; i < results.size(); i++) {
@@ -160,8 +227,25 @@ public class HelloPicking extends SimpleApplication {
     };
 @Override
     public void simpleUpdate(float tpf) {
-       System.out.println("z ="+ z);
-       cube.setLocalTranslation(x, y,z++); 
+       //TODO: add update code
+        if (!isRunning) {
+            guiNode.detachAllChildren();
+            guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
+            BitmapText helloText = new BitmapText(guiFont, false);
+            helloText.setSize(guiFont.getCharSet().getRenderedSize());
+            helloText.setText("PAUSE");
+            helloText.setLocalTranslation(300, helloText.getLineHeight(), 0);
+            guiNode.attachChild(helloText);
+        } else {
+            initCrossHairs();
+        }
+
+        if (isRunning) {
+            for (Spatial s : ninjas.getChildren()) {
+                s.move(tpf, 0, 0);
+               
+            }
+        }
     }
     /**
      * Um objeto de cubo para a prática de destino
@@ -176,7 +260,17 @@ public class HelloPicking extends SimpleApplication {
         cube.setMaterial(mat1);
         return cube;
     }
-
+ public Spatial createNinja(String name) {
+        /**
+         * Load a model. Uses model and texture from jme3-test-data library!
+         */
+        Spatial ninja = assetManager.loadModel("Models/Ninja/Ninja.mesh.xml");
+        ninja.setName(name);
+        ninja.scale(0.025f);
+        ninja.setLocalTranslation(-5.5f, -0.5f, 0);
+        ninja.rotate(0, -FastMath.PI / 2, 0);
+        return ninja;
+    }
     /**
      * Um piso para mostrar que o "tiro" pode passar por vários objetos.
      */
